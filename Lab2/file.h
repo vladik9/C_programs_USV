d1ef63a69b75168d9dd2ef3a7bdf521eaa27ace3
vladik9 <v.cornici@gmail.com>
2021-03-22 19:05:27 +0200

    init

---
+++ Lab2/file.h
@@ -0,0 +1,26 @@
+#ifndef _FILE_H_
+#define _FILE_H_
+#include <stdio.h>
+
+typedef struct produs
+{
+     
+
+} PRODUS;
+
+PRODUS *CitireFisier(char *numeFisier, int *nrProduse);
+
+char *afisareProduse(PRODUS *tab, int n);
+
+//Functie de cautarea a unui produs dupa cod (codX).
+//Functia va returna pozitia produsului gasit din tabelul de produse sau valoarea -1 daca produsul nu a fost gasit
+int cautaProdus(PRODUS *tab, int n, int codX);
+
+//adaugare produs in tabloul de produse - se va realoca spatiu pentru noul produs (transmis in functie printr-un pointer la struct produs)
+PRODUS *adaugaProdus(PRODUS *tab, int *n, PRODUS *x);
+
+//Functie de salvare a produselor intr-un fisier.
+//Functia va returna TRUE daca produsele au fost salvate cu succes, respectiv FALS daca operatia a esuat.
+bool salvareProduseInFisier(char *numeFisier, PRODUS *tab, int n);
+
+#endif // !_FILE_H_
--- Lab2/main.c
+++ Lab2/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "file.h"
 
 int main()
 {
