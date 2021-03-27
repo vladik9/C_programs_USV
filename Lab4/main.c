85fbca7d7aaa88872b467abb69315ca717b08437
Cornici Vlad <49037796+vladik9@users.noreply.github.com>
2021-03-27 19:53:02 +0200

    sleep manipulation

--- Lab4/main.c
+++ Lab4/main.c
@@ -4,7 +4,7 @@
 #include "listai2.c"
 //#include <unistd.h>  // for sleap linux
 #include <Windows.h> // for sleap windows
-
+#define sleep_time 1 //sleap time
 int main()
 {
      int check_condition = 0;
@@ -19,43 +19,46 @@ int main()
      lista_negativa = newl(); //negativ list initialization
      bool save_to_file = false;
      bool list_devided_condion = false;
+
      do
      {
-          sleep(2); // sleep for 3 seconds
-          system("clear");
+
+          sleep(2);      // sleep for 3 seconds
+                         // system("clear"); // for linux only
+          system("cls"); //for windows only
           printf("\n################################\n");
           printf("Bine ai venit in programul meu!!!\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("1 - citeste un numar intreg si - l introduce la inceputul listei.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("?2 - citeste un fisier text ce contine numere intregi separate prin spatii\nsi le introduce in lista (la sfarsitul listei)\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("3 - afiseaza lista.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("?4 |adauga functie| - insereaza un element  in lista dupa un element cu informatia data.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("5 - cauta un element in lista.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("6 - elimina primul element din lista.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("7 - elimina ultimul element din lista.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("?8 - elimina din lista elementul cu informatia x.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("9 - salveaza lista intr-un fisier text.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("10 - din lista L se formeaza doua noi liste, una ce contine elementele\nnegative si una continând elementele pozitive.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("11 - afiseaza lista continand elementele pozitive.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("12 - afiseaza lista continand elementele negative.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("13 - informatii despre autor.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("14 - terminare program.\n");
-          sleep(1);
+          sleep(sleep_time);
           printf("Optiunea ta aici :> ");
-          sleep(1);
+          sleep(sleep_time);
           scanf("%d", &check_condition);
           printf("\n################################\n");
 
@@ -144,7 +147,6 @@ int main()
                printf("Ai introdus o optiune invalida.\n");
                break;
           }
-
      } while (check_condition != 14);
 
      return 0;
